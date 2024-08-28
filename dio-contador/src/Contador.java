1564f9fad50b51e546d46df85ff828973992fed7
yanmullerwk <yan.muller.contact@gmail.com>
2024-08-28 14:27:06 -0300

    exercicio finalizado

---
+++ dio-contador/src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("o primeiro numero deve ser MENOR que o segundo");
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; contagem >= i; i++) {
+            System.out.println("imprimeindo o numero " + i);
+        }
+
+        // realizar o for para imprimir os números com base na variável contagem
+    }
+}
